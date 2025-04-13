3aaf4f33474e9d66fc7305131cc6c5acab8444a5
MosinFAM <alexfilinxxx@mail.ru>
2025-04-13 13:49:32 +0300

    students rychni

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from sqlalchemy import select
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import select, delete, update
 from fastapi.responses import JSONResponse
 from app.logger import logger
 from app.database.database import lifespan, SessionDep
@@ -14,29 +14,6 @@ from app.schemas.schemas import (
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/students", response_model=list[StudentSchema])
-async def get_all_students(session: SessionDep):
-    query = select(StudentModel)
-    result = await session.execute(query)
-    students = result.scalars().all()
-    logger.info("Fetched %d students", len(students))
-    list_of_students = list()
-    for student in students:
-        list_of_students.append({"id": student.id,
-                                 "groupr_id": student.group_id,
-                                 "name": student.name})
-    return list_of_students
-
-
-@app.get("/groups", response_model=list[GroupSchema])
-async def get_all_devices(session: SessionDep):
-    query = select(GroupModel)
-    result = await session.execute(query)
-    groups = result.scalars().all()
-    logger.info("Fetched %d groups", len(groups))
-    return groups
-
-
 @app.post("/students")
 async def create_student(data: StudentSchema, session: SessionDep):
     students = StudentModel(name=data.name,
@@ -48,6 +25,70 @@ async def create_student(data: StudentSchema, session: SessionDep):
     return JSONResponse({"id": students.id,
                          "groupr_id": students.group_id,
                          "name": students.name})
+
+
+@app.get("/students", response_model=list[StudentSchema])
+async def get_all_students(session: SessionDep):
+    query = select(StudentModel)
+    result = await session.execute(query)
+    await session.commit()
+    students = result.scalars().all()
+    logger.info("Fetched %d students", len(students))
+    list_of_students = list()
+    for student in students:
+        list_of_students.append({"id": student.id,
+                                 "groupr_id": student.group_id,
+                                 "name": student.name})
+    return list_of_students
+
+
+@app.get("/students/{student_id}", response_model=list[StudentSchema])
+async def get_students_by_id(student_id: int, session: SessionDep):
+    query = select(StudentModel).where(StudentModel.id == student_id)
+    result = await session.execute(query)
+    await session.commit()
+    student = result.scalar_one_or_none()
+    if not student:
+        raise HTTPException(status_code=404, detail="student not found")
+    return JSONResponse({"id": student.id,
+                         "parent_id": student.group_id,
+                         "name": student.name})
+
+
+@app.put("/students/{student_id}", response_model=list[StudentSchema])
+async def put_students_by_id(student_id: int,
+                             data: StudentSchema,
+                             session: SessionDep):
+    query = update(StudentModel).where(StudentModel.id == student_id).values(
+        name=data.name,
+        email=data.email,
+        group_id=data.group_id
+    )
+    result = await session.execute(query)
+    await session.commit()
+    if result.rowcount == 0:
+        raise HTTPException(status_code=404, detail="student not found")
+    return JSONResponse({"id": student_id,
+                         "parent_id": data.group_id,
+                         "name": data.name})
+
+
+@app.delete("/students/{student_id}", response_model=list[StudentSchema])
+async def delete_students_by_id(student_id: int, session: SessionDep):
+    query = delete(StudentModel).where(StudentModel.id == student_id)
+    result = await session.execute(query)
+    if result.rowcount == 0:
+        raise HTTPException(status_code=404, detail="student not found")
+    return JSONResponse({"message": "deleted"})
+
+
+@app.get("/groups", response_model=list[GroupSchema])
+async def get_all_devices(session: SessionDep):
+    query = select(GroupModel)
+    result = await session.execute(query)
+    groups = result.scalars().all()
+    logger.info("Fetched %d groups", len(groups))
+    return groups
 
 
 @app.post("/groups")
